26b5ca2b741f64d786dd0ecae81b6eeef8d1f408
Kayode Ogunnowo <48393847+dansagam@users.noreply.github.com>
2024-03-10 00:18:45 +0100

    feat: create the select component

--- src/shared/select/Select.tsx
+++ src/shared/select/Select.tsx
@@ -20,54 +20,56 @@ function Select(props: SelectProps) {
   );
   const selectedValue = objValue?.[value];
   return (
-    <Popover className="relative">
-      {({ open, close }) => (
-        <React.Fragment>
-          <Popover.Button>
-            {selectedValue?.icon && showIcon && <selectedValue.icon />}
+    <div>
+      <Popover className="relative">
+        {({ open, close }) => (
+          <React.Fragment>
+            <Popover.Button className=" rounded-[100px] flex  justify-between items-center min-w-24 outline-none p-2 bg-[#f2f2f2]">
+              {selectedValue?.icon && showIcon && <selectedValue.icon />}
+              <span className=" text-black font-semibold">{selectedValue?.value || ""}</span>
 
-            {selectedValue?.value || ""}
-
-            <LuChevronDown data-app-active={open} />
-          </Popover.Button>
-          <Transition
-            as={React.Fragment}
-            enter="transition ease-out duration-200"
-            enterFrom="opacity-0 translate-y-1"
-            enterTo="opacity-100 translate-y-0"
-            leave="transition ease-in duration-150"
-            leaveFrom="opacity-100 translate-y-0"
-            leaveTo="opacity-0 translate-y-1"
-          >
-            <Popover.Panel className="absolute z-10 bg-white rounded-[2rem]">
-              <div>
-                {options.map((el) => (
-                  <li
-                    className={twMerge(
-                      "flex relative justify-between gap-2 px-5 py-6 transition-all duration-200 ",
-                      "after:content-[''] h-0 after:absolute after:w-1 hover:after:h-full after:bg-gray-300 "
-                    )}
-                    onClick={(e) => {
-                      e.stopPropagation();
-                      if (onChange) {
-                        onChange(el.value);
-                      }
-                      close();
-                    }}
-                  >
-                    <span className=" flex items-center justify-start gap-2">
-                      {el.icon && <el.icon />}
-                      <span>{el.label}</span>
-                    </span>
-                    {el.sub && <span>{el.sub}</span>}
-                  </li>
-                ))}
-              </div>
-            </Popover.Panel>
-          </Transition>
-        </React.Fragment>
-      )}
-    </Popover>
+              <LuChevronDown data-app-active={open} />
+            </Popover.Button>
+            <Transition
+              as={React.Fragment}
+              enter="transition ease-out duration-200"
+              enterFrom="opacity-0 translate-y-1"
+              enterTo="opacity-100 translate-y-0"
+              leave="transition ease-in duration-150"
+              leaveFrom="opacity-100 translate-y-0"
+              leaveTo="opacity-0 translate-y-1"
+            >
+              <Popover.Panel className="absolute z-10 bg-white rounded-[2rem]">
+                <div>
+                  {options.map((el, idx) => (
+                    <li
+                      key={idx}
+                      className={twMerge(
+                        "flex relative justify-between gap-2 px-5 py-6 transition-all duration-200 items-center",
+                        "after:content-[''] h-0 after:absolute after:w-1 hover:after:h-full after:bg-gray-300 whitespace-nowrap "
+                      )}
+                      onClick={(e) => {
+                        e.stopPropagation();
+                        if (onChange) {
+                          onChange(el.value);
+                        }
+                        close();
+                      }}
+                    >
+                      <span className=" flex items-center justify-start gap-2">
+                        {el.icon && <el.icon />}
+                        <span>{el.label}</span>
+                      </span>
+                      {el.sub && <span>{el.sub}</span>}
+                    </li>
+                  ))}
+                </div>
+              </Popover.Panel>
+            </Transition>
+          </React.Fragment>
+        )}
+      </Popover>
+    </div>
   );
 }
 
