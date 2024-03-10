c0f9951eda247c2961523c08cdad511721719e21
Kayode Ogunnowo <48393847+dansagam@users.noreply.github.com>
2024-03-10 19:31:45 +0100

    fix: add the modal to the app

--- src/modules/home/views/Transfer.tsx
+++ src/modules/home/views/Transfer.tsx
@@ -5,14 +5,17 @@ import HomeLayout from "@/layout/home-layout";
 import { usePaystackPayment } from "react-paystack";
 import { BASE_ENV } from "@/api/envFile";
 import AppErrorBoundary from "@/shared/error/ErrorBoundary";
+import Dialog from "@/shared/modal/Dialog";
+import React from "react";
 
 function Transfer() {
+  const [open, setOpen] = React.useState(true);
+  const [err, setErr] = React.useState(false);
   const form = useForm({
     defaultValues: transferDefaultValues,
     resolver: transferResolver,
   });
   const receiverAmount = form.watch("receiverAmount");
-  console.log({ receiverAmount });
   const config = {
     reference: new Date().getTime().toString(),
     email: "user@example.com",
@@ -24,9 +27,12 @@ function Transfer() {
     initializePayment({
       onSuccess(values) {
         console.log({ values });
+        setOpen(true);
       },
       onClose(values) {
         console.log({ values });
+        setOpen(true);
+        setErr(true);
       },
     });
   };
@@ -37,6 +43,17 @@ function Transfer() {
         {/** this is isolate the page when it fails and crash */}
         <TransferForm form={form} />
       </AppErrorBoundary>
+      <Dialog
+        open={open}
+        onClose={() => {
+          setOpen(false);
+          setErr(false);
+        }}
+      >
+        <p data-app-error={Boolean(err)} className=" text-2xl text-green-800 data-[app-error=true]:text-red-600">
+          {err ? "Error occurred" : "Transferred successfully"}
+        </p>
+      </Dialog>
     </HomeLayout>
   );
 }
---
+++ src/shared/modal/Dialog.tsx
@@ -0,0 +1,126 @@
+import React from "react";
+import { Transition, Dialog as UDialog } from "@headlessui/react";
+import { IChildren, Prettify } from "@/@types/baseInterface";
+import { ImCancelCircle } from "react-icons/im";
+import { twMerge } from "tailwind-merge";
+
+type DialogProps = Prettify<
+  {
+    open: boolean;
+    onClose(): void;
+    onAction?: () => void;
+    onSecAction?: () => void;
+    title?: string;
+    description?: string;
+    actionText?: string;
+    secActionText?: string;
+    ActionButton?: React.ButtonHTMLAttributes<HTMLButtonElement>;
+    SecActionButton?: React.ButtonHTMLAttributes<HTMLButtonElement>;
+    loading?: boolean;
+    disabled?: boolean;
+    maxWidth?: string;
+  } & IChildren
+>;
+
+const Dialog = (props: DialogProps) => {
+  const {
+    open,
+    onClose,
+    onAction,
+    onSecAction,
+    children,
+    actionText,
+    description,
+    secActionText,
+    title,
+    ActionButton,
+    SecActionButton,
+    disabled,
+    loading,
+    maxWidth,
+  } = props;
+  return (
+    <Transition appear show={open} as={React.Fragment}>
+      <UDialog as="div" className="relative z-[350]" onClose={onClose}>
+        <Transition.Child
+          as={React.Fragment}
+          enter="ease-out duration-300"
+          enterFrom="opacity-0"
+          enterTo="opacity-100"
+          leave="ease-in duration-200"
+          leaveFrom="opacity-100"
+          leaveTo="opacity-0"
+        >
+          <div className="fixed inset-0 bg-black bg-opacity-25" />
+        </Transition.Child>
+        <div className="fixed inset-0 overflow-y-auto">
+          <div className="flex min-h-full items-center justify-center p-4 text-center">
+            <Transition.Child
+              as={React.Fragment}
+              enter="ease-out duration-300"
+              enterFrom="opacity-0 scale-95"
+              enterTo="opacity-100 scale-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100 scale-100"
+              leaveTo="opacity-0 scale-95"
+            >
+              <UDialog.Panel
+                className={twMerge(
+                  "w-full transform overflow-auto relative transition-all rounded-lg shadow-xl bg-white",
+                  maxWidth || "max-w-md"
+                )}
+              >
+                <div className=" flex justify-between items-center py-3 px-6 border-b sticky top-0">
+                  <UDialog.Title className=" flex-1">{title || ""}</UDialog.Title>
+
+                  <button>
+                    <ImCancelCircle />
+                  </button>
+                </div>
+                <UDialog.Description as="div" className="p-6">
+                  {description && <p className=" text-neutral-bodyText text-sm my-2">{description}</p>}
+                  <div>{children}</div>
+                </UDialog.Description>
+                <div className="sticky bottom-0">
+                  <div className="flex justify-center gap-4 items-center py-4 px-6">
+                    <button
+                      className=" bg-primary-light py-3 px-3 rounded-md w-full"
+                      onClick={(e) => {
+                        e.stopPropagation();
+                        if (onSecAction) {
+                          onSecAction();
+                        } else {
+                          onClose();
+                        }
+                      }}
+                      disabled={disabled || loading}
+                      {...SecActionButton}
+                    >
+                      {secActionText || "Cancel"}
+                    </button>
+                    {actionText && (
+                      <button
+                        onClick={(e) => {
+                          e.stopPropagation();
+                          if (onAction) {
+                            onAction();
+                          }
+                        }}
+                        disabled={disabled || loading}
+                        {...ActionButton}
+                      >
+                        {actionText}
+                      </button>
+                    )}
+                  </div>
+                </div>
+              </UDialog.Panel>
+            </Transition.Child>
+          </div>
+        </div>
+      </UDialog>
+    </Transition>
+  );
+};
+
+export default Dialog;
