341b3774edad4c78ef324560a479121c6cbb4c88
Kayode Ogunnowo <48393847+dansagam@users.noreply.github.com>
2024-03-11 06:38:09 +0100

    chore: add the number format function

---
+++ src/utils/numberFormat.ts
@@ -0,0 +1,9 @@
+export const formatNumber = (val: any, dp: number) => {
+  const formatter = new Intl.NumberFormat("ng-NG", {
+    style: "decimal",
+    maximumFractionDigits: dp,
+    minimumFractionDigits: dp,
+  });
+  //   return formatter.format(val);
+  return isNaN(parseFloat(formatter.format(val))) ? "-" : formatter.format(val);
+};
