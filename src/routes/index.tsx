df0b4dcd6fbdb1ddb7043c510dc6cfe4bb96abed
Kayode Ogunnowo <48393847+dansagam@users.noreply.github.com>
2024-03-13 23:20:35 +0100

    fix: add the hashroute

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,35 +1,32 @@
 import React from "react";
-import { createBrowserRouter, Outlet, Navigate } from "react-router-dom";
+import { Outlet, Navigate, createHashRouter } from "react-router-dom";
 import { BASE_PATH, mainRoutes } from "./routes";
 import NotFound from "@/components/NotFound";
 
-const router = createBrowserRouter(
-  [
-    {
-      path: "/",
-      element: <Navigate to={BASE_PATH.HOME} />,
-    },
-    {
-      path: "/",
-      element: (
-        <React.Suspense
-          fallback={<div className="w-screen h-screen flex justify-center items-center bg-white">Loading...</div>}
-        >
-          <Outlet />
-        </React.Suspense>
-      ),
-      children: mainRoutes,
-    },
-    {
-      path: "404",
-      element: <NotFound />,
-    },
-    {
-      path: "*",
-      element: <NotFound />,
-    },
-  ],
-  { basename: "/gird-finance" }
-);
+const router = createHashRouter([
+  {
+    path: "/",
+    element: <Navigate to={BASE_PATH.HOME} />,
+  },
+  {
+    path: "/",
+    element: (
+      <React.Suspense
+        fallback={<div className="w-screen h-screen flex justify-center items-center bg-white">Loading...</div>}
+      >
+        <Outlet />
+      </React.Suspense>
+    ),
+    children: mainRoutes,
+  },
+  {
+    path: "404",
+    element: <NotFound />,
+  },
+  {
+    path: "*",
+    element: <NotFound />,
+  },
+]);
 
 export default router;
