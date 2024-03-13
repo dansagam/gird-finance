317b802695c1ae2f6ffca28edc6063c99891fc7c
Kayode Ogunnowo <48393847+dansagam@users.noreply.github.com>
2024-03-13 23:11:44 +0100

    fic: add the basename

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,30 +3,33 @@ import { createBrowserRouter, Outlet, Navigate } from "react-router-dom";
 import { BASE_PATH, mainRoutes } from "./routes";
 import NotFound from "@/components/NotFound";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Navigate to={BASE_PATH.HOME} />,
-  },
-  {
-    path: "/",
-    element: (
-      <React.Suspense
-        fallback={<div className="w-screen h-screen flex justify-center items-center bg-white">Loading...</div>}
-      >
-        <Outlet />
-      </React.Suspense>
-    ),
-    children: mainRoutes,
-  },
-  {
-    path: "404",
-    element: <NotFound />,
-  },
-  {
-    path: "*",
-    element: <NotFound />,
-  },
-]);
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <Navigate to={BASE_PATH.HOME} />,
+    },
+    {
+      path: "/",
+      element: (
+        <React.Suspense
+          fallback={<div className="w-screen h-screen flex justify-center items-center bg-white">Loading...</div>}
+        >
+          <Outlet />
+        </React.Suspense>
+      ),
+      children: mainRoutes,
+    },
+    {
+      path: "404",
+      element: <NotFound />,
+    },
+    {
+      path: "*",
+      element: <NotFound />,
+    },
+  ],
+  { basename: "/gird-finance" }
+);
 
 export default router;
